df3eca14b1a9390c1ba7ed24f021ad7c23b8c54d
Roland Mackintosh <rolandmackintosh@outlook.com>
2022-01-24 22:16:44 -0800

    installed nodemon and ordered handleUser funcs alphabetically

--- data/resolvers/handleUser.js
+++ data/resolvers/handleUser.js
@@ -1,18 +1,18 @@
-async function getUsers(context) {
-  const { knex } = context;
-
-  return await knex('testusers');
-}
-
 async function getUser(args, context) {
   const { id, email } = args;
   const { knex } = context;
-  
+
   const payload = /@/.test(email) ? { email } : { id: +id };
 
-  const userRecord = await knex('testusers').where(payload);
+  const userRecord = await knex("testusers").where(payload);
 
   return userRecord[0];
+}
+
+async function getUsers(context) {
+  const { knex } = context;
+
+  return await knex("testusers");
 }
 
 module.exports = { getUser, getUsers };
