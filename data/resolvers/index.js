33a4ebac3ec6d2849606274c2330ff04421459e2
Roland Mackintosh <rolandmackintosh@outlook.com>
2022-01-25 22:08:06 -0800

    resolved a few bracket related bugs

--- data/resolvers/index.js
+++ data/resolvers/index.js
@@ -1,15 +1,32 @@
-const { getUser, getUsers } = require('./handleUser');
-
-
 const resolvers = {
   Query: {
-    user(parent, args, context, info) {
-      return getUser(args, context); 
+    async user(parent, args, context, info) {
+      // please note, this isn't cleaning up your code, as much as it is  making it easier for me to process my own mutation
+      const { id, email } = args;
+      const { knex } = context;
+
+      const payload = /@/.test(email) ? { email } : { id: +id };
+
+      const userRecord = await knex("testusers").where(payload);
+
+      return userRecord[0];
     },
-    users(parent, args, context, info) {
-      return getUsers(context); 
-    }
+    async users(parent, args, context, info) {
+      const { knex } = context;
+
+      return await knex("testusers");
+    },
+  },
+  Mutation: {
+    changeUserInfo(root, args, context, info) {
+      return db.students.create({
+        first_name: args.first_name,
+        last_name: args.last_name,
+        email: args.email,
+        country: args.country,
+      });
+    },
   },
 };
 
-module.exports = [ resolvers ];
+module.exports = [resolvers];
--- data/types/index.js
+++ data/types/index.js
@@ -13,15 +13,15 @@ const typeDefs = gql`
     users: [User]
     user(id: ID, email: String): User
   }
+
+  type Mutation {
+    changeUserInfo(
+      first_name: String
+      last_name: String
+      email: String
+      country: String
+    ): User
+  }
 `;
 
 module.exports = [typeDefs];
-
-const storage = `
-mutation UpdateUser {
-  id: ID!
-  first_name: String
-  last_name: String
-  email: String
-  country: String
-}`;
